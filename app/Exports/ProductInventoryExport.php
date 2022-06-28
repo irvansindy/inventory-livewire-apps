4c32b857b18aabe4f61ad65501cb83993c3eccc4
irvansindy <irvanmuhammad22@gmail.com>
2022-06-28 11:24:48 +0700

    fix data export excel inventory

--- app/Exports/ProductInventoryExport.php
+++ app/Exports/ProductInventoryExport.php
@@ -14,11 +14,27 @@ class ProductInventoryExport implements FromCollection, WithCustomCsvSettings, W
     */
     public function collection()
     {
-        return ProductInventory::with([
-            'products:productName',
-            'supplier:supplierName',
-            'user:name'
-        ])->get();
+        return ProductInventory::join(
+            'products', 'products.id', '=', 'product_inventories.productId',
+            )->join(
+                'suppliers', 'suppliers.id', '=', 'product_inventories.productOrigin',
+            )->join(
+                'users', 'users.id', '=', 'product_inventories.sertificateMaker',
+            )
+            ->get([
+            'product_inventories.inventoryCode', 
+            'products.productName',
+            'products.merk',
+            'product_inventories.serialNumber', 
+            'product_inventories.purchasingNumber', 
+            'suppliers.supplierName',
+            'product_inventories.productPrice', 
+            'product_inventories.registeredDate', 
+            'product_inventories.yearOfEntry', 
+            'product_inventories.yearOfUse', 
+            'product_inventories.productStatus', 
+            'users.name'
+        ]);
     }
 
     public function getCsvSettings(): array
@@ -31,17 +47,18 @@ class ProductInventoryExport implements FromCollection, WithCustomCsvSettings, W
     public function headings(): array
     {
         return [
-            'Inventory Code',
+            'Inventory Code', //inventory table
             'Product Name',
             'Merk',
-            'Serial Number',
-            'Purchasing Number',
-            'Product Supplier',
+            'Serial Number', //inventory table
+            'Purchasing Number', //inventory table
+            'Product Supplier', 
             'Price',
-            'Register Date',
-            'Year of Entry',
-            'Year of Use',
-            'Status',
+            'Register Date', //inventory table
+            'Year of Entry', //inventory table
+            'Year of Use', //inventory table
+            'Status', //inventory table
+            'Sertificate Maker' //inventory table
         ];
     }
 }
